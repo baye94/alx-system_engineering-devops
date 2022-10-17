88d1d8238ee8b9ba83ac1a510ad05508e5798af3
baye94 <fallcheikh893@gmail.com>
2022-10-17 22:23:19 +0000

    0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,25 @@
-
 #!/usr/bin/python3
-"""
-A Python script that, using this REST API,
-for a given employee ID, returns information about his/her TODO list progress
-"""
-from argparse import ArgumentParser
-from os import path
-from requests import get
-from sys import argv
+""" Script that uses JSONPlaceholder API to get information about employee """
+import requests
+import sys
 
 
-mockApi = "https://jsonplaceholder.typicode.com/"
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
 
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
-if __name__ == '__main__':
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
 
-    parser = ArgumentParser(prog=path.basename(argv[0]))
-    parser.add_argument('ID', type=int, help="employee ID")
-    args = parser.parse_args()
-    user = get('/'.join([mockApi, 'users', str(args.id)])).json()
-    todo = get('/'.join([mockApi, 'todos']), params={'userId': args.id}).json()
-    completed = [task for task in todo if task['NUMBER_OF_DONE_TASKS'] is True]
-    print('EMPLOYEE_NAME {} is done with tasks({}/{}):'.format(
-        user['EMPLOYEE_NAME'], len(completed), len(todo)))
-    print('\n'.join('\t {}'.format(task['title']) for task in completed))
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
