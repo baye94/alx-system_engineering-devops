9ab2ba6e42fb7c3e3927a75c4778e63eb6ee345e
baye94 <fallcheikh893@gmail.com>
2022-11-06 04:06:41 +0000

    fix bugs

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,3 +3,19 @@ exec { 'wsd3':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
   path    =>  ['/bin', '/usr/bin']
 }
+
+
+server {     
+    listen 80;      
+    listen [::]:80;      
+    server_name http://BANQUE_AGRICOLE_IPRES_FRONT_END.com;      
+    root /var/www/BANQUE_AGRICOLE_IPRES_FRONT_END/dist/LBA;   
+    server_tokens off;   
+    index index.html index.htm;     
+ 
+    location / {         
+        # First attempt to server request as file, then         
+        # as directory, then fall back to displaying a 404.          
+        try_files $uri $uri/ /index.html =404;      
+    }
+}
